ec704fed44466573f0964ac2ed6285f46650fee4
AndersonClayton <andersonclaytondesenvolvedor@gmail.com>
2022-04-27 17:19:15 -0300

    Tentativa de modificar a imagem de fundo

---
+++ js/modificacoes.js
@@ -0,0 +1,5 @@
+function mudaImagem() {
+    window.alert("Funcionando!");
+    document.getElementsByClassName('imgPrinciapl').style.backgroundImage = "url('imgBanner/The Umbrella Academy.jpg')";
+}
+
